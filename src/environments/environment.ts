1f4462da564841a6863c1c94ade38f0731bf0ce2
Mong <mongmang.vak@gmail.com>
2023-04-05 23:23:43 +0700

    cha

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  API_URL: 'http://203.159.93.121:3000/api',
+  API_URL: 'http://141.98.19.5:3000/api',
   //API_URL: 'http://localhost:3000/api'
   //API_URL: $ENV.API_URL
 };

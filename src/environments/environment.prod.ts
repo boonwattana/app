6d4272e5d6c7d05c93ac45223f141f208785e8e8
Mong <mongmang.vak@gmail.com>
2023-07-30 20:25:15 +0700

    edit api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: true,
-  API_URL: 'http://141.98.19.5:3000/api',
-  //API_URL: 'http://localhost:3000/api'
+  // API_URL: 'http://49.229.56.21:3000/api',
+  API_URL: 'http://:::3000/api'
 };
